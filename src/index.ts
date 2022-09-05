94ee1a776d2ce2409341d4772372c61b6e6d08fc
Wolftallemo <72576136+Wolftallemo@users.noreply.github.com>
2022-09-05 01:47:17 +0000

    Create api

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,155 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `wrangler dev src/index.ts` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `wrangler publish src/index.ts --name my-worker` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { Request as IRequest, Router } from "itty-router";
 
 export interface Env {
-  // Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-  // MY_KV_NAMESPACE: KVNamespace;
-  //
-  // Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-  // MY_DURABLE_OBJECT: DurableObjectNamespace;
-  //
-  // Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-  // MY_BUCKET: R2Bucket;
+  API_KEYS: KVNamespace;
+  INTERNAL_KEY: string;
+  VERIFICATIONS: KVNamespace;
+}
+
+interface APIKey {
+  access_level: number;
+  created_at: number;
+  creator: string;
+}
+
+interface User {
+  id: number;
+  username: string;
+  privacy?: {
+    discord: number;
+    roblox: number;
+  };
+}
+
+async function getAPIKeyHash(apiKey: string) {
+  return Array.from(
+    new Uint8Array(
+      await crypto.subtle.digest("SHA-512", new TextEncoder().encode(apiKey))
+    )
+  )
+    .map((b) => b.toString(16).padStart(2, "0"))
+    .join("");
+}
+
+function makeResponse(body: { [k: string]: any }, status: number): Response {
+  return new Response(JSON.stringify(body), {
+    headers: {
+      "content-type": "application/json",
+    },
+    status,
+  });
 }
 
 export default {
-  async fetch(
-    request: Request,
-    env: Env,
-    ctx: ExecutionContext
-  ): Promise<Response> {
-    return new Response("Hello World!");
+  async fetch(request: Request, env: Env, ctx: ExecutionContext) {
+    const router = Router();
+    const apiKey = request.headers.get("authorization");
+
+    router.get("/api/discord/:id", async (req: IRequest & Request) => {
+      const key: User | null = await env.VERIFICATIONS.get(
+        req.params?.id as string,
+        "json"
+      );
+
+      if (!key) return makeResponse({ error: "User is not verified" }, 404);
+
+      const privacyLevel = key.privacy?.discord as number;
+
+      delete key.privacy;
+
+      if (!privacyLevel) return makeResponse(key, 200);
+
+      if (!apiKey)
+        return makeResponse({ error: "Resource requires API key" }, 401);
+
+      const keyHash = await getAPIKeyHash(apiKey);
+      const apiKeyData: APIKey | null = await env.API_KEYS.get(keyHash, "json");
+
+      if (!apiKeyData && apiKey !== env.INTERNAL_KEY)
+        return makeResponse({ error: "API key is invalid" }, 401);
+
+      if (
+        apiKey !== env.INTERNAL_KEY &&
+        privacyLevel > (apiKeyData?.access_level as number)
+      )
+        return makeResponse({ error: "You cannot access this user" }, 403);
+
+      return makeResponse(key, 200);
+    });
+
+    router.get("/api/roblox/:id", async (req: IRequest & Request) => {
+      const key: string[] | null = await env.VERIFICATIONS.get(
+        req.params?.id as string,
+        "json"
+      );
+
+      if (!key)
+        return makeResponse(
+          { error: "No Discord accounts linked to this Roblox account" },
+          404
+        );
+
+      const keysToVerify = [];
+      const keysToReturn = [];
+
+      for (const uid of key) {
+        const locatedUser: User | null = await env.VERIFICATIONS.get(
+          uid,
+          "json"
+        );
+
+        if (!locatedUser) continue;
+
+        if (!locatedUser.privacy?.roblox) {
+          delete locatedUser.privacy;
+          keysToReturn.push(uid);
+        } else {
+          keysToVerify.push(uid);
+        }
+      }
+
+      if (keysToVerify.length && apiKey) {
+        const apiKeyHash = await getAPIKeyHash(apiKey);
+        const apiKeyData: APIKey | null = await env.API_KEYS.get(
+          apiKeyHash,
+          "json"
+        );
+
+        if (!apiKeyData && apiKey !== env.INTERNAL_KEY)
+          return makeResponse({ error: "API key is invalid" }, 401);
+
+        for (const pendingKey of keysToVerify) {
+          const reverseKey: User | null = await env.VERIFICATIONS.get(
+            pendingKey,
+            "json"
+          );
+
+          if (!reverseKey) continue;
+
+          if (
+            apiKey === env.INTERNAL_KEY ||
+            (reverseKey.privacy?.roblox as number) <=
+              (apiKeyData?.access_level as number)
+          ) {
+            delete reverseKey.privacy;
+            keysToReturn.push(reverseKey);
+          }
+
+          if (!keysToReturn.length)
+            return makeResponse(
+              { error: "You cannot access the Discord accounts of this user" },
+              403
+            );
+
+          return makeResponse(keysToReturn, 200);
+        }
+      } else if (!keysToReturn.length && !apiKey) {
+        return makeResponse({ error: "Resource requires API key" }, 401);
+      } else {
+        return makeResponse(keysToReturn, 200);
+      }
+    });
+
+    router.all("*", () => makeResponse({ error: "Not found" }, 404));
   },
 };

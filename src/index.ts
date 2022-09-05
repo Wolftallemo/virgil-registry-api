1e7364319c9791752a2cae055a931807abbf0a43
Wolftallemo <72576136+Wolftallemo@users.noreply.github.com>
2022-09-05 01:54:41 +0000

    Move router outside of fetch

--- src/index.ts
+++ src/index.ts
@@ -40,116 +40,121 @@ function makeResponse(body: { [k: string]: any }, status: number): Response {
   });
 }
 
-export default {
-  async fetch(request: Request, env: Env, ctx: ExecutionContext) {
-    const router = Router();
-    const apiKey = request.headers.get("authorization");
+const router = Router();
 
-    router.get("/api/discord/:id", async (req: IRequest & Request) => {
-      const key: User | null = await env.VERIFICATIONS.get(
-        req.params?.id as string,
-        "json"
+router.get(
+  "/api/discord/:id",
+  async (req: IRequest & Request, env: Env, _context: ExecutionContext) => {
+    const apiKey = req.headers.get("authorization");
+    const key: User | null = await env.VERIFICATIONS.get(
+      req.params?.id as string,
+      "json"
+    );
+
+    if (!key) return makeResponse({ error: "User is not verified" }, 404);
+
+    const privacyLevel = key.privacy?.discord as number;
+
+    delete key.privacy;
+
+    if (!privacyLevel) return makeResponse(key, 200);
+
+    if (!apiKey)
+      return makeResponse({ error: "Resource requires API key" }, 401);
+
+    const keyHash = await getAPIKeyHash(apiKey);
+    const apiKeyData: APIKey | null = await env.API_KEYS.get(keyHash, "json");
+
+    if (!apiKeyData && apiKey !== env.INTERNAL_KEY)
+      return makeResponse({ error: "API key is invalid" }, 401);
+
+    if (
+      apiKey !== env.INTERNAL_KEY &&
+      privacyLevel > (apiKeyData?.access_level as number)
+    )
+      return makeResponse({ error: "You cannot access this user" }, 403);
+
+    return makeResponse(key, 200);
+  }
+);
+
+router.get(
+  "/api/roblox/:id",
+  async (req: IRequest & Request, env: Env, _context: ExecutionContext) => {
+    const key: string[] | null = await env.VERIFICATIONS.get(
+      req.params?.id as string,
+      "json"
+    );
+
+    if (!key)
+      return makeResponse(
+        { error: "No Discord accounts linked to this Roblox account" },
+        404
       );
 
-      if (!key) return makeResponse({ error: "User is not verified" }, 404);
+    const keysToVerify = [];
+    const keysToReturn = [];
 
-      const privacyLevel = key.privacy?.discord as number;
+    const apiKey = req.headers.get("authorization");
 
-      delete key.privacy;
+    for (const uid of key) {
+      const locatedUser: User | null = await env.VERIFICATIONS.get(uid, "json");
 
-      if (!privacyLevel) return makeResponse(key, 200);
+      if (!locatedUser) continue;
 
-      if (!apiKey)
-        return makeResponse({ error: "Resource requires API key" }, 401);
+      if (!locatedUser.privacy?.roblox) {
+        delete locatedUser.privacy;
+        keysToReturn.push(uid);
+      } else {
+        keysToVerify.push(uid);
+      }
+    }
 
-      const keyHash = await getAPIKeyHash(apiKey);
-      const apiKeyData: APIKey | null = await env.API_KEYS.get(keyHash, "json");
+    if (keysToVerify.length && apiKey) {
+      const apiKeyHash = await getAPIKeyHash(apiKey);
+      const apiKeyData: APIKey | null = await env.API_KEYS.get(
+        apiKeyHash,
+        "json"
+      );
 
       if (!apiKeyData && apiKey !== env.INTERNAL_KEY)
         return makeResponse({ error: "API key is invalid" }, 401);
 
-      if (
-        apiKey !== env.INTERNAL_KEY &&
-        privacyLevel > (apiKeyData?.access_level as number)
-      )
-        return makeResponse({ error: "You cannot access this user" }, 403);
-
-      return makeResponse(key, 200);
-    });
-
-    router.get("/api/roblox/:id", async (req: IRequest & Request) => {
-      const key: string[] | null = await env.VERIFICATIONS.get(
-        req.params?.id as string,
-        "json"
-      );
-
-      if (!key)
-        return makeResponse(
-          { error: "No Discord accounts linked to this Roblox account" },
-          404
-        );
-
-      const keysToVerify = [];
-      const keysToReturn = [];
-
-      for (const uid of key) {
-        const locatedUser: User | null = await env.VERIFICATIONS.get(
-          uid,
+      for (const pendingKey of keysToVerify) {
+        const reverseKey: User | null = await env.VERIFICATIONS.get(
+          pendingKey,
           "json"
         );
 
-        if (!locatedUser) continue;
+        if (!reverseKey) continue;
 
-        if (!locatedUser.privacy?.roblox) {
-          delete locatedUser.privacy;
-          keysToReturn.push(uid);
-        } else {
-          keysToVerify.push(uid);
+        if (
+          apiKey === env.INTERNAL_KEY ||
+          (reverseKey.privacy?.roblox as number) <=
+            (apiKeyData?.access_level as number)
+        ) {
+          delete reverseKey.privacy;
+          keysToReturn.push(reverseKey);
         }
-      }
 
-      if (keysToVerify.length && apiKey) {
-        const apiKeyHash = await getAPIKeyHash(apiKey);
-        const apiKeyData: APIKey | null = await env.API_KEYS.get(
-          apiKeyHash,
-          "json"
-        );
-
-        if (!apiKeyData && apiKey !== env.INTERNAL_KEY)
-          return makeResponse({ error: "API key is invalid" }, 401);
-
-        for (const pendingKey of keysToVerify) {
-          const reverseKey: User | null = await env.VERIFICATIONS.get(
-            pendingKey,
-            "json"
+        if (!keysToReturn.length)
+          return makeResponse(
+            { error: "You cannot access the Discord accounts of this user" },
+            403
           );
 
-          if (!reverseKey) continue;
-
-          if (
-            apiKey === env.INTERNAL_KEY ||
-            (reverseKey.privacy?.roblox as number) <=
-              (apiKeyData?.access_level as number)
-          ) {
-            delete reverseKey.privacy;
-            keysToReturn.push(reverseKey);
-          }
-
-          if (!keysToReturn.length)
-            return makeResponse(
-              { error: "You cannot access the Discord accounts of this user" },
-              403
-            );
-
-          return makeResponse(keysToReturn, 200);
-        }
-      } else if (!keysToReturn.length && !apiKey) {
-        return makeResponse({ error: "Resource requires API key" }, 401);
-      } else {
         return makeResponse(keysToReturn, 200);
       }
-    });
+    } else if (!keysToReturn.length && !apiKey) {
+      return makeResponse({ error: "Resource requires API key" }, 401);
+    } else {
+      return makeResponse(keysToReturn, 200);
+    }
+  }
+);
 
-    router.all("*", () => makeResponse({ error: "Not found" }, 404));
-  },
+router.all("*", () => makeResponse({ error: "Not found" }, 404));
+
+export default {
+  fetch: router.handle,
 };
